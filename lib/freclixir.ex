33ffd95ffdb37c947359b7ba9f79e1394331e575
Jamie Schembri <jamie@lico.nl>
2017-04-12 21:10:07 +0200

    Initial commit

---
+++ lib/freclixir.ex
@@ -0,0 +1,18 @@
+defmodule Freclixir do
+  @moduledoc """
+  Documentation for Freclixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Freclixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/freclixir_test.exs
@@ -0,0 +1,8 @@
+defmodule FreclixirTest do
+  use ExUnit.Case
+  doctest Freclixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
